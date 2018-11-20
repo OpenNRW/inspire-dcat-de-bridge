8fa64094af7005d6042a627cec66ac09acd70d46
einspanier <u.einspanier@conterra.de>
2018-11-20 10:08:06 +0100

    upgraded Spring and Camel dependencies

--- src/main/java/eu/odp/harvest/geo/oai/xslt/HttpAwareUriResolver.java
+++ src/main/java/eu/odp/harvest/geo/oai/xslt/HttpAwareUriResolver.java
@@ -2,10 +2,6 @@ package eu.odp.harvest.geo.oai.xslt;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.xml.XsltUriResolver;
-import org.apache.camel.spi.ClassResolver;
-import org.apache.camel.util.FileUtil;
-import org.apache.camel.util.ObjectHelper;
-import org.apache.camel.util.ResourceHelper;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
 import org.apache.commons.httpclient.methods.GetMethod;
@@ -13,42 +9,23 @@ import org.apache.log4j.Logger;
 
 import javax.xml.transform.Source;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.URIResolver;
 import javax.xml.transform.stream.StreamSource;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 /**
- * Resolves a document reference for protcols "http" or "https" in an XSL document with HTTP client.
- * For all other URIs uses the default rsolver base don classpath.
+ * Resolves a document reference for protocols "http" or "https" in an XSL document with HTTP client.
+ * For all other URIs uses the default resolver base don classpath.
  */
-public class HttpAwareUriResolver implements URIResolver {
+public class HttpAwareUriResolver extends XsltUriResolver {
 
     private final static Logger LOG = Logger.getLogger(HttpAwareUriResolver.class);
 
     private static HttpClient httpClient = new HttpClient(new MultiThreadedHttpConnectionManager());
 
-    @Override
-    public Source resolve(String href, String base) throws TransformerException {
-        if (ObjectHelper.isEmpty(href)) {
-            throw new TransformerException("include href is empty");
-        } else {
-            LOG.debug("Resolving URI with href: " + href);
-            if (href.startsWith("http:") || href.startsWith("https:")) {
-                try {
-                    return resolveHttp(href);
-                } catch (Exception e) {
-                    throw new TransformerException(e);
-                }
-            } else {
-                String scheme = ResourceHelper.getScheme(href);
-                href = href.startsWith("/") ? href : "/" + href;
-                return new StreamSource(getClass().getResourceAsStream(href));
-            }
-        }
+    public HttpAwareUriResolver(CamelContext context, String location) {
+        super(context, location);
     }
-/*
+
     public Source resolve(String href, String base) throws TransformerException {
         if (href.startsWith("http:") || href.startsWith("https:")) {
             try {
@@ -63,7 +40,6 @@ public class HttpAwareUriResolver implements URIResolver {
             return super.resolve(href, base);
         }
     }
-*/
 
     private Source resolveHttp(String href) throws Exception {
         GetMethod method = new GetMethod(href);
@@ -71,7 +47,6 @@ public class HttpAwareUriResolver implements URIResolver {
             httpClient.executeMethod(method);
             byte[] bytes = method.getResponseBody();
             return new StreamSource(new ByteArrayInputStream(bytes));
-//            return new StreamSource(method.getResponseBodyAsStream());
         }
         finally {
             method.releaseConnection();
--- src/main/java/eu/odp/harvest/geo/oai/xslt/HttpAwareUriResolverFactory.java
+++ src/main/java/eu/odp/harvest/geo/oai/xslt/HttpAwareUriResolverFactory.java
@@ -1,14 +1,12 @@
 package eu.odp.harvest.geo.oai.xslt;
 
 import org.apache.camel.CamelContext;
-//import org.apache.camel.builder.xml.XsltUriResolver;
-//import org.apache.camel.component.xslt.DefaultXsltUriResolverFactory;
+import org.apache.camel.component.xslt.XsltUriResolverFactory;
 
 import javax.xml.transform.URIResolver;
 
-public class HttpAwareUriResolverFactory  {
+public class HttpAwareUriResolverFactory implements XsltUriResolverFactory {
     public URIResolver createUriResolver(CamelContext camelContext, String resourceUri) {
-//        return new HttpAwareUriResolver(camelContext, resourceUri);
-        return null;
+        return new HttpAwareUriResolver(camelContext, resourceUri);
     }
 }
